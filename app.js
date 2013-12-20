662626f50e67ac102e1dfb8fd5874f17fe62a26e
James Mortemore <james.mortemore@clock.co.uk>
2013-12-20 10:40:18 +0000

    Initial commit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@
 var express = require('express')
   , http = require('http')
   , path = require('path')
+  , async = require('async')
+  , freshdeskTicketCount = require('./lib/freshdesk-ticket-count')
   , app = express()
 
 // all environments
@@ -25,8 +27,32 @@ if ('development' === app.get('env')) {
   app.use(express.errorHandler())
 }
 
+var freshdeskUrl = process.env.FRESHDESK_URL
+  , ticketCountTypes = process.env.COUNT_TYPES ? JSON.parse(process.env.COUNT_TYPES) : []
+
+
 app.get('/', function (req, res) {
   res.render('index', { title: 'Express' })
+})
+
+app.get('/ticket-counts', function (req, res) {
+
+  async.map(ticketCountTypes, function (type, cb) {
+
+    freshdeskTicketCount(freshdeskUrl, type, function (error, count) {
+      if (error) return cb(error)
+
+      var stat = { type: type, count: count }
+
+      cb(error, stat)
+    })
+
+  }, function (error, stats) {
+    if (error) console.warn(error)
+
+    res.json(stats)
+  })
+
 })
 
 http.createServer(app).listen(app.get('port'), function(){
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,20 @@
-(function () {})()
+(function () {
+
+  var request = new XMLHttpRequest()
+  request.onreadystatechange = function () {
+    var res = JSON.parse(request.response)
+
+    res.forEach(function (stats) {
+      document.getElementsByClassName('js-ticket-count-' + stats.type)[0].innerHTML = stats.count
+    })
+  }
+
+  request.open('GET', '/ticket-counts', true)
+  request.send()
+
+  setInterval(function () {
+    request.open('GET', '/ticket-counts', true)
+    request.send()
+  }, 300000)
+
+})()
